3fad233ba56d775eb0f579fdf84d12cfd722ea81
seop7 <seungsub7@naver.com>
2018-01-17 00:04:38 +0900

    update

--- README.md
+++ README.md
@@ -7,12 +7,17 @@ TODO LIST SERVER
 * MongoDB(+Mongoose)
 ## 3. 사용환경
 * 서버 PORT : process.env.PORT || 8080;
-* MongoDB 접속정보
-  * 'mongodb://localhost:27017/todoApp'
-  *  PORT : 27017
-  * 컬렉션 : todoApp
+* MongoDB 접속정보('mongodb://localhost:27017/todoApp')
+  * PORT : 27017
+  * DATABASE : todoApp
 * API 정보(REST API)
   * GET(READ) : '/api/todolist'
   * POST(CREATE) : '/api/todolist'
   * PUT(UPDATE) : '/api/todolist/:todo' <<업데이트 예정>>
   * DELETE : '/api/todolist/:todo' <<업데이트 예정>>
+* 스키마 정보(Model)
+  * grade: Number (중요도)
+  * contents: String (내용)
+  * registration_date: { type: Date, defulat: Date.now } (등록일 / 서버시간 자동입력)
+  * update_date: { type: Date, default: Date.now } (수정일 / 서버시간 자동입력)
+  * isComplete: Boolean (완료여부)
--- route.js
+++ route.js
@@ -9,13 +9,13 @@ module.exports = (app, Todo) => {
         });
     });
 
-    // CREATE Todo
+    // CREATE ToDo
     app.post('/api/todolist', (req, res) => {
         const todo = new Todo();
         todo.grade = req.body.grade;
         todo.contents = req.body.contents;
-        todo.registration_date = (req.body.registration_date)? new Date(req.body.registration_date) : new Date();
-        todo.update_date = (req.body.update_date)? new Date(req.body.update_date) : new Date();
+        todo.registration_date = new Date();
+        todo.update_date = new Date();
         todo.isComplete = false;
 
         todo.save((err) => {
@@ -27,5 +27,36 @@ module.exports = (app, Todo) => {
 
             res.json({result: 1});
         });
+    });
+
+    // UPDATE ToDo
+    app.put('/api/todolist/:todo', (req, res) => {
+        Todo.findById(req.params.todo, (err, todo) => {
+            if(err) return res.status(500).json({ error: 'database failure' });
+            if(!todo) return res.status(404).json({ error: 'todo not found' });
+
+            if(req.body.grade) todo.grade = req.body.grade;
+            if(req.body.contents) todo.contents = req.body.contents;
+            if(req.body.isComplete) todo.isComplete = req.body.isComplete;
+            todo.update_date = new Date();
+
+            todo.save((err) => {
+                if(err) res.status(500).json({error: 'failed to update'});
+                res.json({ message: 'todo updated' });
+            });
+        });
+    });
+
+    // DELETE ToDo
+    app.delete('/api/todolist/:todo', (req, res) => {
+        Todo.remove({_id: req.params.todo}, (err, output) => {
+            if(err) return res.status(500).json({ error: "database failure" });
+
+            /* ( SINCE DELETE OPERATION IS IDEMPOTENT, NO NEED TO SPECIFY )
+                if(!output.result.n) return res.status(404).json({ error: "todo not found" });
+                res.json({ message: "todo deleted" });
+            */
+            res.status(204).end();
+        })
     });
 }
